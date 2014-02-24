f6343a6545eaf4ca08737d244c90a8439729f1e0
Stefan Illgen <stefan.illgen.ebenheit@gmail.com>
2014-02-25 00:16:52 +0100

    Rotable Checkbox implemented (but still property not bound)

--- src/main/java/org/devel/jfxcontrols/scene/control/skin/RotatableCheckBoxSkin.java
+++ src/main/java/org/devel/jfxcontrols/scene/control/skin/RotatableCheckBoxSkin.java
@@ -3,6 +3,10 @@
  */
 package org.devel.jfxcontrols.scene.control.skin;
 
+import java.util.List;
+
+import javafx.css.CssMetaData;
+import javafx.css.Styleable;
 import javafx.scene.control.CheckBox;
 
 import com.sun.javafx.scene.control.skin.CheckBoxSkin;
@@ -17,5 +21,18 @@ public class RotatableCheckBoxSkin extends CheckBoxSkin {
 		super(checkbox);
 	}
 
+	@Override
+	public List<CssMetaData<? extends Styleable, ?>> getCssMetaData() {
+		return CheckBox.getClassCssMetaData();
+	}
+	
+//	/** 
+//     * @return The CssMetaData associated with this class, which may include the
+//     * CssMetaData of its super classes.
+//     */    public static List<CssMetaData<? extends Styleable, ?>> getClassCssMetaData() {
+//        return SkinBase.StyleableProperties.STYLEABLES;
+//    }
+
+	
 	
 }
